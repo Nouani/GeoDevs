a7b951a616157bc3be83dff57a8c5624e4ca2015
Nouani <nouanigabriel@hotmail.com>
2020-01-15 17:16:52 -0300

    /web: Formulário de cadastro + estilização

--- web/src/App.js
+++ web/src/App.js
@@ -1,24 +1,113 @@
 import React from 'react';
-import logo from './logo.svg';
+
+import './global.css';
 import './App.css';
+import './Sidebar.css';
+import './Main.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app">
+      <aside>
+        <strong>Cadastrar</strong>
+        <form action="">
+          <div className="input-block">
+            <label htmlFor="github_username">Usuário do GitHub</label>
+            <input
+              type="text"
+              name="github_username"
+              id="github_username"
+              required
+            />
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="techs">Tecnologias</label>
+            <input
+              type="text"
+              name="techs"
+              id="techs"
+              required
+            />
+          </div>
+
+          <div className="input-group">
+            <div className="input-block">
+              <label htmlFor="latitude">Latitude</label>
+              <input
+                type="text"
+                name="latitude"
+                id="latitude"
+                required
+              />
+            </div>
+
+            <div className="input-block">
+              <label htmlFor="longitude">Longitude</label>
+              <input
+                type="text"
+                name="longitude"
+                id="longitude"
+                required
+              />
+            </div>
+          </div>
+          
+          <button type="submit">Salvar</button>
+        </form>
+      </aside>
+
+      <main>
+        <ul>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars1.githubusercontent.com/u/49238044?v=4" alt="Nouani"/>
+              <div className="user-info">
+                <strong>Nouani</strong>
+                <span>ReactJS, React Native, NodeJS</span>
+              </div>
+              <p>IT student/monitor at Technical High School of Campinas - Unicamp</p>
+              <a href="https://github.com/Nouani">Acessar perfil no GitHub</a>
+            </header>
+          </li>
+
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars1.githubusercontent.com/u/49238044?v=4" alt="Nouani"/>
+              <div className="user-info">
+                <strong>Nouani</strong>
+                <span>ReactJS, React Native, NodeJS</span>
+              </div>
+              <p>IT student/monitor at Technical High School of Campinas - Unicamp</p>
+              <a href="https://github.com/Nouani">Acessar perfil no GitHub</a>
+            </header>
+          </li>
+
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars1.githubusercontent.com/u/49238044?v=4" alt="Nouani"/>
+              <div className="user-info">
+                <strong>Nouani</strong>
+                <span>ReactJS, React Native, NodeJS</span>
+              </div>
+              <p>IT student/monitor at Technical High School of Campinas - Unicamp</p>
+              <a href="https://github.com/Nouani">Acessar perfil no GitHub</a>
+            </header>
+          </li>
+
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars1.githubusercontent.com/u/49238044?v=4" alt="Nouani"/>
+              <div className="user-info">
+                <strong>Nouani</strong>
+                <span>ReactJS, React Native, NodeJS</span>
+              </div>
+              <p>IT student/monitor at Technical High School of Campinas - Unicamp</p>
+              <a href="https://github.com/Nouani">Acessar perfil no GitHub</a>
+            </header>
+          </li>
+        </ul>
+      </main>
     </div>
   );
 }
