4ca72d0c6f61e0774ee186c5f3152481c2559724
Nouani <nouanigabriel@hotmail.com>
2020-01-16 15:40:47 -0300

    /mobile: Barra de pesquisa + acesso a web pelo app

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,16 +1,17 @@
 import React, { useState, useEffect } from 'react';
-import { View, StyleSheet, Image, Text } from 'react-native';
+import { View, StyleSheet, Image, Text, TextInput, TouchableOpacity } from 'react-native';
 import MapView, { Marker, Callout } from 'react-native-maps';
 import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location'
+import { MaterialIcons } from '@expo/vector-icons';
 
-function Main({ navigation }){
+function Main({ navigation }) {
     const [currentRegion, setCurrentRegion] = useState(null);
 
     useEffect(() => {
-        async function loadInitialPosition(){
+        async function loadInitialPosition() {
             const { granted } = await requestPermissionsAsync();
-        
-            if (granted){
+
+            if (granted) {
                 const { coords } = await getCurrentPositionAsync({
                     enableHighAccuracy: true,
                 });
@@ -29,25 +30,44 @@ function Main({ navigation }){
         loadInitialPosition();
     }, []);
 
-    if (!currentRegion){
+    if (!currentRegion) {
         return null;
     }
 
     return (
-        <MapView style={styles.map} initialRegion={currentRegion}>
-            <Marker coordinate={{ latitude: -22.8968408, longitude: -47.0780913 }}>
-                <Image style={styles.avatar} source={{ uri: 'https://avatars2.githubusercontent.com/u/2254731?v=4' }} />
-                <Callout onPress={() => {
-                    navigation.navigate('Profile', { github_username: 'diego3g' })
-                }}>
-                    <View style={styles.callout}>
-                        <Text style={styles.devName}>Diego Fernandes</Text>
-                        <Text style={styles.devBio}>CTO na @Rocketseat. Apaixonado pelas melhores tecnologias de desenvolvimento web e mobile.</Text>
-                        <Text style={styles.devTechs}>ReactJS, React Native, NodeJS</Text>
-                    </View>
-                </Callout>
-            </Marker>
-        </MapView>
+        <>
+            <MapView style={styles.map} initialRegion={currentRegion}>
+                <Marker coordinate={{ latitude: -22.8968408, longitude: -47.0780913 }}>
+                    <Image style={styles.avatar} source={{ uri: 'https://avatars2.githubusercontent.com/u/2254731?v=4' }} />
+                    <Callout onPress={() => {
+                        navigation.navigate('Profile', { github_username: 'diego3g' })
+                    }}>
+                        <View style={styles.callout}>
+                            <Text style={styles.devName}>Diego Fernandes</Text>
+                            <Text style={styles.devBio}>CTO na @Rocketseat. Apaixonado pelas melhores tecnologias de desenvolvimento web e mobile.</Text>
+                            <Text style={styles.devTechs}>ReactJS, React Native, NodeJS</Text>
+                        </View>
+                    </Callout>
+                </Marker>
+            </MapView>
+            <View style={styles.searchForm}>
+                <TextInput
+                    style={styles.searchInput}
+                    placeholder='Buscar devs por techs...'
+                    placeholderTextColor="#999"
+                    autoCapitalize="words"
+                    autoCorrect={false}
+                />
+
+                <TouchableOpacity onPress={() => { }} style={styles.loadButton}>
+                    <MaterialIcons
+                        name="my-location"
+                        size={20}
+                        color="#FFF"
+                    />
+                </TouchableOpacity>
+            </View>
+        </>
     )
 }
 
@@ -75,7 +95,40 @@ const styles = StyleSheet.create({
     },
     devTechs: {
         marginTop: 5
-    }
+    },
+    searchForm: {
+        position: 'absolute',
+        top: 20,
+        left: 20,
+        right: 20,
+        zIndex: 5,
+        flexDirection: 'row',
+    },
+    searchInput: {
+        flex: 1,
+        height: 50,
+        backgroundColor: '#FFF',
+        color: '#333',
+        borderRadius: 25,
+        paddingHorizontal: 20,
+        fontSize: 16,
+        shadowColor: '#000',
+        shadowOpacity: 0.2,
+        shadowOffset: {
+            width: 4,
+            height: 4
+        },
+        elevation: 9
+    },
+    loadButton: {
+        width: 50,
+        height: 50,
+        backgroundColor: '#8E4DFF',
+        borderRadius: 25,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 15
+    },
 })
 
 export default Main;
