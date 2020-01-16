085ab50fdbbc78c939e9a331a72f0391a14f64e4
Nouani <nouanigabriel@hotmail.com>
2020-01-16 19:55:06 -0300

    /web: Botões para edição e remoção de dev's + estilização

--- web/src/components/DevItem/index.js
+++ web/src/components/DevItem/index.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import EditOutlinedIcon from '@material-ui/icons/EditOutlined';
+import ClearOutlinedIcon from '@material-ui/icons/ClearOutlined';
 
 import './styles.css';
 
@@ -6,10 +8,22 @@ function DevItem({ dev }) {
     return (
         <li className="dev-item">
             <header>
-                <img src={dev.avatar_url} alt={dev.name} />
-                <div className="user-info">
-                    <strong>{dev.name}</strong>
-                    <span>{dev.techs.join(', ')}</span>
+                <div className="inside-block">
+                    <img src={dev.avatar_url} alt={dev.name} />
+                
+                    <div className="user-info">
+                        <strong>{dev.name}</strong>
+                        <span>{dev.techs.join(', ')}</span>
+                    </div>
+
+                    <div className="icons">
+                        <button className="edit-button">
+                            <EditOutlinedIcon className="icon" />
+                        </button>
+                        <button className="remove-button">
+                            <ClearOutlinedIcon className="icon" />
+                        </button>
+                    </div>
                 </div>
                 <p>{dev.bio}</p>
                 <a href={`https://github.com/${dev.github_username}`}>Acessar perfil no GitHub</a>
